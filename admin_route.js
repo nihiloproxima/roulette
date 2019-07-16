c61f21a5c86014ec6f02c0bcb250910c0795f902
Nathan <naplouvi@student.le-101.fr>
2019-07-16 15:13:36 +0200

    up

--- admin_route.js
+++ admin_route.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const User = require("./schemas/User");
 
 adminRouter.get('/', async (req, res) => {
-	if (req.session.login == "nihilo") {
+	if (req.session.login == "nihilo" || req.session.login == "ftourret") {
 		res.sendFile(__dirname + '/views/admin');
 	} else {
 		res.send("mdr noob");
