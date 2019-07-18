5755fe33a7189480dee93d767bbcc37147b2f0ac
Nathan <naplouvi@student.le-101.fr>
2019-07-18 14:54:18 +0200

    update

--- middlewares/pointsManager.js
+++ middlewares/pointsManager.js
@@ -1,48 +1,49 @@
 const axios = require('axios');
 
-const pointsManager = function (user_id, points) {
+const pointsManager = async function (user_id, points) {
 	console.log("Grabbing access token...");
-	axios
+	const request_token = await axios
 		.post("https://api.intra.42.fr/oauth/token", {
 			grant_type: "client_credentials",
 			client_id: process.env.STAFF_ID,
 			client_secret: process.env.STAFF_SECRET,
 			scope: "public projects tig"
-		})
-		.then(response => {
-			const access_token = response.data.access_token;
-			console.log("Access granted, code : ", access_token);
-			console.log("Grabbing coallition infos and coallition_user");
-			axios
-				.get('https://api.intra.42.fr/v2/coalitions_users?user_id=' + user_id)
-				.then(results => {
-					console.log("Done.");
-					coalition_user = results.data[0];
-					axios.post('https://api.intra.42.fr/v2/coalitions/' + coalition_user.coalition_id + '/scores', {
-						score: {
-							coalition_user_id: coalition_user.user_id,
-							reason: 'You played, you won.',
-							value: points
-						}
-					}, {
-						headers: {
-							"Authorization": "Bearer " + access_token
-						}
-					}).then(call => {
-						console.log("Points attribued.")
-						return (0);
-					}).catch(error => {
-						console.log("Can't close :", error);
-						return (error);
-					})
-				})
-				.catch(error => {
-					return (error);
-				})
-		})
-		.catch(error => {
-			console.log(error);
-		})
+		});
+	console.log("request log :", request_token);
+	const access_token = request_token.data.access_token;
+	console.log("Access token granted : ", access_token);
+	if (access_token) {
+		console.log("Grabbing coallition infos and coallition_user");
+		const coalition_user = await axios
+			.get('https://api.intra.42.fr/v2/coalitions_users?user_id=' + user_id);
+		if (coalition_user) {
+			coalition_user = coalition_user[0];
+			axios.post('https://api.intra.42.fr/v2/coalitions/' + coalition_user.coalition_id + '/scores', {
+				score: {
+					coalition_user_id: coalition_user.user_id,
+					reason: 'You played, you won.',
+					value: points
+				}
+			}, {
+				headers: {
+					"Authorization": "Bearer " + access_token
+				}
+			}).then(response => {
+				console.log("Done.");
+				console.log("Points attribued.")
+				return (0);
+			}).catch(error => {
+				console.log("Problem giving coalition poinnts :", error);
+				return (error);
+			})
+		} else {
+			console.log("No coalition_user...");
+			return (1);
+		}
+	} else {
+		console.log("No access token...");
+		return (1);
+	}
 }
 
 module.exports = pointsManager;
