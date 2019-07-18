5c36f8e055aa2e186d18b92e8155e657b7c8fdff
Nathan <naplouvi@student.le-101.fr>
2019-07-18 15:27:05 +0200

    update

--- middlewares/pointsManager.js
+++ middlewares/pointsManager.js
@@ -9,7 +9,6 @@ const pointsManager = async function (user_id, points) {
 			client_secret: process.env.STAFF_SECRET,
 			scope: "public projects tig"
 		});
-	console.log("request log :", request_token);
 	const access_token = request_token.data.access_token;
 	console.log("Access token granted : ", access_token);
 	if (access_token) {
@@ -20,13 +19,13 @@ const pointsManager = async function (user_id, points) {
 					"Authorization": "Bearer " + access_token
 				}
 			});
-		console.log("Coalition_user : ", coalition_user.data);
 		if (coalition_user.data) {
-			coalition_user = coalition_user.data[0];
+			user = coalition_user.data[0];
+			console.log("Coalition_user : ", user);
 			console.log("Posting new score...");
-			axios.post('https://api.intra.42.fr/v2/coalitions/' + coalition_user.coalition_id + '/scores', {
+			axios.post('https://api.intra.42.fr/v2/coalitions/' + user.coalition_id + '/scores', {
 				score: {
-					coalition_user_id: coalition_user.id,
+					coalition_user_id: user.id,
 					reason: 'You played, you won.',
 					value: points
 				}
