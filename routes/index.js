97716f88fce43f091a3694f2ad47fedde08ff415
Nathan <naplouvi@student.le-101.fr>
2019-08-03 14:05:11 +0200

    up

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ const appRoutes = require('./appRoutes');
 const apiRoutes = require('./apiRoutes');
 const adminRoutes = require('./adminRoutes');
 
-router.use('/', appRoutes);
-router.use('/api', apiRoutes);
-router.use('/admin', adminRoutes);
+// router.use('/', appRoutes);
+// router.use('/api', apiRoutes);
+// router.use('/admin', adminRoutes);
 router.get('*', (req, res) => {
-	res.status(404).send("Nope.");
+	res.status(404).send("La roulette prend du repos.");
 	console.log(req.session.login, " requested : ", req.protocol + '://' + req.get('host') + req.originalUrl);
 })
 module.exports = router;
